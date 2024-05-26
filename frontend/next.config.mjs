a1225c1490ebf937f3d97af33f6755f5f6fc725f
FirebenderYT <FirebenderYT@gmail.com>
2024-05-26 15:30:03 +0300

    fix: + Added security headers and fixed bug with no image attachments

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,60 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  // @ts-ignore
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' ${
+                process.env.NODE_ENV == "production" ? "" : "'unsafe-eval'"
+              } https://www.google.com/recaptcha/api.js https://www.gstatic.com/;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data:;
+              font-src 'self';
+              connect-src 'self' ${
+                process.env.NEXT_PUBLIC_HTTP_GRAPHQL_ENDPOINT
+              } ${process.env.NEXT_PUBLIC_WS_GRAPHQL_ENDPOINT};
+              object-src 'none';
+              frame-src https://www.google.com/;
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+            `.replace(/\n/g, ""),
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_DOMAIN,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
+  poweredByHeader: false,
   reactStrictMode: true,
   env: {
-    httpGraphQlEndpoint: process.env.HTTP_GRAPHQL_ENDPOINT,
-    wsGraphQlEndpoint: process.env.WS_GRAPHQL_ENDPOINT,
+    httpGraphQlEndpoint: process.env.NEXT_PUBLIC_HTTP_GRAPHQL_ENDPOINT,
+    wsGraphQlEndpoint: process.env.NEXT_PUBLIC_WS_GRAPHQL_ENDPOINT,
     googleRecaptchaPublicKey: process.env.GOOGLE_RECAPTCHA_PUBLIC_KEY,
   },
   images: {
@@ -12,7 +63,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.BLOB_HOSTNAME,
+        // @ts-ignore
+        hostname: process.env.NEXT_PUBLIC_BLOB_HOSTNAME,
         port: "",
         pathname: "/**",
       },
